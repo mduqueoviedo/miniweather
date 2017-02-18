3cb557a7f7337569c9b1634cb9041b08870f95ca
Marcos Duque <mduqueoviedo@gmail.com>
2017-02-18 03:48:17 +0100

    Set up mongodb correctly

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -48,14 +48,14 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-  #DatabaseCleaner.strategy = :truncation
+  DatabaseCleaner.strategy = :truncation
 
   config.before(:each) do
-    #DatabaseCleaner.start
+    DatabaseCleaner.start
   end
 
   config.after(:each) do
-    #DatabaseCleaner.clean
+    DatabaseCleaner.clean
   end
 
   Capybara.javascript_driver = :poltergeist
