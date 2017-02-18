4237cd6fe4312e0706bc348e81579f79ad0e580e
Marcos Duque <mduqueoviedo@gmail.com>
2017-02-18 14:57:39 +0100

    Showing results

--- app/assets/javascripts/controllers/home.controller.js
+++ app/assets/javascripts/controllers/home.controller.js
@@ -2,25 +2,29 @@ angular.module('miniWeatherApp').controller('HomeController', [
     '$scope', '$http',
     function ($scope, $http) {
       $scope.search = {};
+      $scope.error = false;
 
-      $scope.submit = function() {
-        var url = '/api/query_weather?query=' + $scope.search.city;
+      $scope.getWeather = function(isRandom) {
+        $scope.weatherResult = null;
+        $scope.error = false;
+        var url = isRandom ? '/api/query_weather?random=true' : '/api/query_weather?query=' + $scope.search.city;
 
         $http.get(url).then(function(response) {
-          console.log('Ok ' + response);
+          var res = response['data']['result'];
+          if (res['status']) {
+            $scope.weatherResult = res;
+            console.log(res);
+          } else {
+            console.log(res);
+            $scope.error = true;
+            $scope.error_message = res['error_message'];
+          }
         }).catch(function(response) {
-          console.log('Error: ' + response);
+          res = response['data']['result'];
+          $scope.error = true;
+          $scope.error_message = res['error_message'];
+          console.log(res);
         });
       }
-
-      $scope.getRandomLocation = function() {
-        var url = '/api/query_weather?random=true';
-
-        $http.get(url).then(function(response) {
-          console.log('Ok ' + response);
-        }).catch(function(response) {
-          console.log('Error: ' + response);
-        });
-      };
     }
 ]);
--- app/services/weather_query.rb
+++ app/services/weather_query.rb
@@ -13,13 +13,13 @@ class WeatherQuery
     weather_query =
       if is_random
         rng = Random.new
-        lat = rng.rand(-90.0..90.0)
-        lon = rng.rand(-180.0..180.0)
+        lat = rng.rand(-90.0..90.0).round(3)
+        lon = rng.rand(-180.0..180.0).round(3)
 
         "#{@host}#{Settings.weather_api.weather_coordinates_lat_path}" \
           "#{lat}#{Settings.weather_api.weather_coordinates_lon_path}#{lon}"
       else
-        "#{@host}#{Settings.weather_api.weather_query_path}user_query"
+        "#{@host}#{Settings.weather_api.weather_query_path}#{user_query}"
       end
     # Add units and api key
     weather_query += Settings.weather_api.units_query + @units if %w(metric imperial).include?(@units.downcase)
@@ -27,13 +27,14 @@ class WeatherQuery
 
     begin
       uri = URI.parse(weather_query)
+      p uri
       response = Net::HTTP.get_response(uri)
       json_response = JSON.parse(response.body)
 
-      store_log(user_query, json_response)
+      store_log(is_random ? 'random' : user_query, json_response)
       process_response(json_response)
     rescue StandardError => error
-      store_log(user_query, error.to_s)
+      store_log(is_random ? 'random' : user_query, error.to_s)
       {
         status: false,
         error_message: error
@@ -47,20 +48,34 @@ class WeatherQuery
     WeatherLog.create!(
       user_query: user_query,
       raw_result: response,
-      api_res_code: response['code']
+      api_res_code: response['cod']
     )
   end
 
   def process_response(response)
     case response['cod']
     when 200
-      { status: true }
+      {
+        status: true,
+        weather_main: response['weather'].first['main'],
+        weather_description: response['weather'].first['description'],
+        weather_icon: response['weather'].first['icon'],
+        temp: response['main']['temp'],
+        temp_max: response['main']['temp_max'],
+        temp_min: response['main']['temp_min'],
+        wind_speed: response['wind']['speed'],
+        wind_direction: response['wind']['deg'],
+        city: response['name'],
+        country: response['sys']['country'],
+        latitude: response['coord']['lat'],
+        longitude: response['coord']['long']
+      }
     when 502
       { status: false, error_message: 'City not found' }
     when 401
       { status: false, error_message: 'Unauthorized' }
     else
-      { status: false, error_message: "Unexpected error: #{response['error']}" }
+      { status: false, error_message: "Unexpected api response: #{response['cod']}" }
     end
   end
 end
